4f85b86da148022abec3dc3044a5bfe3c5e338eb
linocrie <zaqaryanliana14@gmail.com>
2021-04-05 23:24:54 +0400

    changing db class using inheritance

--- db.php
+++ db.php
@@ -1,25 +1,20 @@
 <?php
 
-class DB
+class DB extends mysqli
 {
-    private $mysqli;
     private $where_condition;
     function  __construct()
     {
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "internship";
-        $this->mysqli = new mysqli($servername, $username, $password, $dbname);
+        parent::__construct('localhost', 'root', '', 'internship');
 
-        if ($this->mysqli->connect_errno) {
-            die("Connection failed: " . $this->mysqli->connect_error);
+        if (mysqli_connect_errno()) {
+            die("Connection failed: " . mysqli_connect_error());
         }
     }
     public function select($sql, $all = true)
     {
         $arr = [];
-        $result = $this->mysqli->query($sql);
+        $result = parent::query($sql);
 
         if (!$all) {
             $result->fetch_assoc();
@@ -33,10 +28,10 @@ class DB
     {
         $data_val = '';
         foreach ($data as $key => $value) {
-            $data_val .= "'" . $this->mysqli->real_escape_string(htmlspecialchars($value)) . "', ";
+            $data_val .= "'" . parent::real_escape_string(htmlspecialchars($value)) . "', ";
         }
         $data_val = substr($data_val, 0, -2);
-        $query = $this->mysqli->query("INSERT INTO $tbl_name (" . implode(",", array_keys($data)) . ")
+        $query = parent::query("INSERT INTO $tbl_name (" . implode(",", array_keys($data)) . ")
         VALUES ($data_val)");
         return $query;
     }
@@ -44,15 +39,16 @@ class DB
     {
         $set = '';
         foreach ($fields as $key => $value) {
-            $set .= $key . "='" . $this->mysqli->real_escape_string(htmlspecialchars($value)) . "', ";
+            $set .= $key . "='" . parent::real_escape_string(htmlspecialchars($value)) . "', ";
         }
         $set = substr($set, 0, -2);
         $where = '';
         if ($this->where_condition) {
             $where = " WHERE " . $this->where_condition;
         }
+        $this->where_condition = '';
         $query = "UPDATE " . $tbl_name . " SET " . $set . $where;
-        $upd_res = $this->mysqli->query($query);
+        $upd_res = parent::query($query);
         return $upd_res;
     }
     public function delete($tbl_name, $where_condition)
@@ -61,8 +57,9 @@ class DB
         if ($this->where_condition) {
             $where = " WHERE " . $this->where_condition;
         }
+        $this->where_condition = '';
         $query = "DELETE FROM " . $tbl_name . $where;
-        $del_res =  $this->mysqli->query($query);
+        $del_res = parent::query($query);
         return $del_res;
     }
     public function where($column, $value, $oper = '=')
